7fc4d72d6ae80601418d3918be731e286dc2112e
Leslie Wubbel <lesliemwubbel@gmail.com>
2022-09-17 18:47:46 -0400

    fix async out of squence

--- sweepa-app/api/src/lib/googlesheet.js
+++ sweepa-app/api/src/lib/googlesheet.js
@@ -5,14 +5,12 @@ const fs = require('fs')
 // we need sheet ID from the
 const { PRIVATE_KEY } = process.env
 const { CLIENT_EMAIL } = process.env
-
-function delay(time) {
-  return new Promise(resolve => setTimeout(resolve, time));
-}
-
 export async function insertEmail(email, Status) {
   const doc = new GoogleSpreadsheet('1MBfmJowTVqVhCyQa1hJByk57xVzaJngEJmpGX1fvAmg');
 
+
+  console.log("were inside", email)
+  console.log("were inside", Status)
   // const creds = require('./peak-comfort-361602-c2bd1bc6d886.json');
   // let creds = fs.readFileSync('peak-comfort-361602-c2bd1bc6d886.json', 'utf-8')
   // console.log("creds",creds)
@@ -63,12 +61,17 @@ export async function insertEmail(email, Status) {
 //  await doc.useServiceAccountAuth(creds);
 // Initialize Auth - see https://theoephraim.github.io/node-google-spreadsheet/#/getting-started/authentication
 
+await doc.useServiceAccountAuth(serviceCredential);
+await doc.loadInfo(); // loads document properties and worksheets
+console.log(doc.title);
+const sheet = doc.sheetsByIndex[0]; // or use doc.sheetsById[id] or doc.sheetsByTitle[title]
+console.log(sheet.title);
+console.log(sheet.rowCount);
+// const sheet.addSheet({ headerValues: ['email', 'Status'] })
+
 try {
   // Try to run this code
-  await doc.useServiceAccountAuth(serviceCredential);
-  const docStatus = await doc.loadInfo(); // loads document properties and worksheets
-  console.log(docStatus)
-  console.log("Test UserServiceAccountAuth", doc.title);
+  const larryRow = await sheet.addRow({  email:  email, Status:Status });
 }
 catch(err) {
   // if any error, Code throws the error
@@ -76,27 +79,6 @@ catch(err) {
 }
 
 
-
-try {
-  // Try to run this code
-  delay(1000).then(() => console.log('ran after 1 second1 passed'));
-  const sheet = doc.sheetsByIndex[0]; // or use doc.sheetsById[id] or doc.sheetsByTitle[title]
-  console.log("Google sheet title", sheet.title);
-  console.log("Row Count", sheet.rowCount);
-  console.log("were inside", email)
-  console.log("were inside", Status)
-  sheet.addRow({  email:  email, Status:Status });
-}
-catch(err) {
-  // if any error, Code throws the error
-  console.log(err)
-}
-
-
-return {
-  headers: {},
-  statuscode: 200,
-  body: JSON.stringify('Success 200')
-}
+return "Sucess"
 }
 
